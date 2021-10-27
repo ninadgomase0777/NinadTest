e0a792342a96e354bc68d1c02e16e1368de1c12c
ninadgomase0777 <66063577+ninadgomase0777@users.noreply.github.com>
2021-10-27 20:29:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
-res.send('Testing Node JS Application');
+res.send('Test');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
