282ef6c9667d6a955702753fd704d9315ad574dc
ninadgomase0777 <66063577+ninadgomase0777@users.noreply.github.com>
2021-11-01 15:46:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello World');
+res.send('Baba Tatya');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
